82b4b7bf8b0ec7b9f45adc7c34a1edb41f8614d2
matspe24 <matspe24@bergen.org>
2022-04-02 13:19:28 -0400

    Achievements.cs added

---
+++ Assets/Scripts/Achievements.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievements : MonoBehaviour
+{
+
+    int achv1 = 0;
+    int achv2 = 0;
+    int achv3 = 0;
+    int achv4 = 0;
+    public GameObject a1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Image img1 = GameObject.Find("Achievements/Canvas/Panel/Panel").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Duck.maxAlt > 50){
+            achv1 = 1;
+        }
+        if(Duck.maxAlt > 200){
+            achv2 = 1;
+        }
+        if(Duck.maxAlt > 500){
+            achv3 = 1;
+        }
+        if(Duck.maxAlt > 650){
+            achv4 = 1;
+        }
+
+    }
+
+}
